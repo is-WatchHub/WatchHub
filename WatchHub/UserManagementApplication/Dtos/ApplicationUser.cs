d2d255a031c8af94369743f3f4b0b9279e3a4d30
DoctorWater <malckov.schura2015@gmail.com>
2024-10-23 00:47:53 +0300

    Revert "feat: first iteration of user management module added"
    This reverts commit 96ecee3698d4ac026684e55140403a363a0d21c2.

--- WatchHub/UserManagementApplication/Dtos/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace UserManagementApplication.Dtos;
-
-public class ApplicationUser : IdentityUser
-{
-}
--- WatchHub/UserManagementApplication/Dtos/CreateUserDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserManagementApplication.Dtos;
-
-public class CreateUserDto
-{
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
