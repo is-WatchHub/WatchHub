50cfb89535a81a4e5f1151ce1d7271cbef21c160
georgedem <georgiidemyanov@yandex.ru>
2024-10-22 03:35:39 +0300

    (#8) add: video player

---
+++ WatchHub/MoviesDomain/VideoPlayer.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesDomain;
+
+public class VideoPlayer
+{
+    public Guid Id { get; set; }
+    public string ContentUrl { get; set; }
+    public VideoPlayerType PlayerType { get; set; }
+}
