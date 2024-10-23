01d7e20f4c4c7e2fd218ba9c390bfc1972114946
georgedem <georgiidemyanov@yandex.ru>
2024-10-24 00:54:23 +0300

    (#8) add: movie media domain model

---
+++ WatchHub/MoviesDomain/MovieMedia.cs
@@ -0,0 +1,8 @@
+﻿namespace MoviesDomain;
+
+public class MovieMedia
+{
+    public Guid Id { get; set; }
+    public string ContentUrl { get; set; }
+    public string CoverImageUrl { get; set; }
+}
