9a47e7775a55842b3f6fe735be373930a72dae57
Nikolay Zadneprovskiy <zadneprovskiywork@gmail.com>
2024-10-30 13:52:45 +0300

    fix: IMovieRepository methods' return types

--- WatchHub/MoviesApplication/Repositories/IMovieRepository.cs
+++ WatchHub/MoviesApplication/Repositories/IMovieRepository.cs
@@ -3,7 +3,7 @@ using MoviesDomain;
 public interface IMovieRepository
 {
     Task<Movie> GetByIdAsync(Guid id);
-    Task<IList<Movie>> GetAllAsync();
+    Task<IEnumerable<Movie>> GetAllAsync();
     Task<Movie> GetRandomAsync();
     Task<Movie> CreateAsync(Movie movie);
 }
