4a7749cd900ca7add42658739d54d1dcc79c6232
irmar <i.r.mark14@gmail.com>
2024-11-04 13:22:14 +0300

    (#23) add: добавил  константы для таблицы movie, рефакторинг MovieConfiguration

--- WatchHub/Infrastructure/MovieConfiguration.cs
+++ WatchHub/Infrastructure/MovieConfiguration.cs
@@ -8,16 +8,39 @@ namespace Infrastructure
     {
         public void Configure(EntityTypeBuilder<Movie> builder)
         {
-            builder.ToTable(Constants.MOVIE_TABLE_NAME);
+            builder.ToTable(MovieTableConstants.MOVIE_TABLE_NAME);
             builder.HasKey(x => x.Id);
-            builder.Property(x => x.Id).HasColumnName(Constants.MOVIE_ID_COLUMN_NAME).IsRequired();
-            builder.Property(x => x.Title).HasColumnName(Constants.MOVIE_TITLE_COLUMN_NAME).HasMaxLength(45).IsRequired();
-            builder.Property(x => x.Description).HasColumnName(Constants.MOVIE_DESCRIPTION_COLUMN_NAME).HasMaxLength(150).IsRequired();
-            builder.Property(x => x.Genre).HasColumnName(Constants.MOVIE_GENRE_COLUMN_NAME).HasConversion<int>().IsRequired();
+
+            builder.Property(x => x.Id)
+                .HasColumnName(MovieTableConstants.MOVIE_ID_COLUMN_NAME)
+                .IsRequired();
+
+            builder.Property(x => x.Title)
+                .HasColumnName(MovieTableConstants.MOVIE_TITLE_COLUMN_NAME)
+                .HasMaxLength(MovieTableConstants.MOVIE_TITLE_COLUMN_MAX_LENGTH)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+                .HasColumnName(MovieTableConstants.MOVIE_DESCRIPTION_COLUMN_NAME)
+                .HasMaxLength(MovieTableConstants.MOVIE_DESCRIPTION_COLUMN_MAX_LENGTH)
+                .IsRequired();
+
+            builder.Property(x => x.Genre)
+                .HasColumnName(MovieTableConstants.MOVIE_GENRE_COLUMN_NAME)
+                .HasConversion<string>()
+                .IsRequired();
+
             builder.OwnsOne(x => x.Media, optionsBuilder =>
             {
-                optionsBuilder.Property(x => x.ContentUrl).HasColumnName(Constants.MOVIE_CONTENT_URL_COLUMN_NAME).HasMaxLength(150).IsRequired();
-                optionsBuilder.Property(x => x.CoverImageUrl).HasColumnName(Constants.MOVIE_COVER_IMAGE_URL_COLUMN_NAME).HasMaxLength(150).IsRequired();
+                optionsBuilder.Property(x => x.ContentUrl)
+                    .HasColumnName(MovieTableConstants.MOVIE_CONTENT_URL_COLUMN_NAME)
+                    .HasMaxLength(MovieTableConstants.MOVIE_CONTENT_URL_COLUMN_MAX_LENGTH)
+                    .IsRequired();
+                
+                optionsBuilder.Property(x => x.CoverImageUrl)
+                    .HasColumnName(MovieTableConstants.MOVIE_COVER_IMAGE_URL_COLUMN_NAME)
+                    .HasMaxLength(MovieTableConstants.MOVIE_COVER_IMAGE_URL_COLUMN_MAX_LENGTH)
+                    .IsRequired();
             });
         }
     }
