00cf0fb00ef93cb6a745d82fd9d7f7025bb2fd4b
irmar <i.r.mark14@gmail.com>
2024-11-04 14:34:35 +0300

    (#23) fix: убрал строку подключения из appsettings, добавил проверку на её наличие в Program

--- WatchHub/Presentation/Program.cs
+++ WatchHub/Presentation/Program.cs
@@ -3,9 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var defaultConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(defaultConnection))
+{
+    throw new ArgumentNullException(nameof(defaultConnection), "Connection string cannot be null or empty.");
+}
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnection"]);
+    options.UseNpgsql(defaultConnection);
 });
 
 builder.Services.AddEndpointsApiExplorer();
