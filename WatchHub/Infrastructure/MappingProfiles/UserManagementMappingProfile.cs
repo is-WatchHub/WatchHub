ca18978cc063c4c5555d1dbe520bf1e405d651b1
Telushkin Artem <artem.telushkin@yandex.ru>
2024-11-08 00:57:52 +0300

    (#16) fix: some fixes in UserManagementMappingProfile

--- WatchHub/Infrastructure/MappingProfiles/UserManagementMappingProfile.cs
+++ WatchHub/Infrastructure/MappingProfiles/UserManagementMappingProfile.cs
@@ -11,24 +11,16 @@ public class UserManagementMappingProfile : Profile
         CreateMap<CreateUserDto, User>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(_ => Guid.NewGuid()))
             .ForMember(dest => dest.Login, opt => opt.MapFrom(src => src.Name))
-            .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => HashPassword(src.Password)))
+            .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
             .ForMember(dest => dest.Role, opt => opt.MapFrom(_ => Role.User));
-        
+
         CreateMap<LoginDto, User>()
             .ForMember(dest => dest.Login, opt => opt.MapFrom(src => src.Name))
-            .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => HashPassword(src.Password)));
-
-        CreateMap<User, CreateUserDto>()
-            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Login))
-            .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-            .ForMember(dest => dest.Password, opt => opt.Ignore());
+            .ForMember(dest => dest.PasswordHash, opt => opt.MapFrom(src => src.Password));
         
         CreateMap<User, LoginDto>()
             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Login))
-            .ForMember(dest => dest.Password, opt => opt.Ignore());
-
+            .ForMember(dest => dest.Password, opt => opt.MapFrom(src=>src.PasswordHash));
     }
-
-    private static string HashPassword(string password) => password;
 }
