41e78255c067a5a13f66553ea699dbd974131656
DoctorWater <malckov.schura2015@gmail.com>
2024-11-07 22:43:58 +0300

    add: interfaces, models and service for verification added

---
+++ WatchHub/Infrastructure/Services/UserManagementService.cs
@@ -0,0 +1,35 @@
+﻿using Infrastructure.Models;
+using Microsoft.AspNetCore.Identity;
+using UserManagementApplication.Dtos;
+using UserManagementApplication.Interfaces;
+
+namespace Infrastructure.Services;
+
+public class UserManagementService : IUserManagementService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+
+    public UserManagementService(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+    {
+        _userManager = userManager;
+        _signInManager = signInManager;
+    }
+    
+    public async Task Login(LoginDto model) => 
+        await _signInManager.PasswordSignInAsync(model.Name, model.Password, model.RememberMe, false);
+    
+    public async Task Logout() =>
+        await _signInManager.SignOutAsync();
+    
+    
+    public async Task CreateUser(CreateUserDto dto)
+    {
+        var user = new ApplicationUser
+        {
+            UserName = dto.Name,
+            Email = dto.Email
+        };
+        await _userManager.CreateAsync(user, dto.Password);
+    }
+}
---
+++ WatchHub/UserManagementApplication/IUserManagementService.cs
@@ -0,0 +1,11 @@
+﻿
+using UserManagementApplication.Dtos;
+
+namespace UserManagementApplication.Interfaces;
+
+public interface IUserManagementService
+{
+    public Task Login(LoginDto model);
+    public Task Logout();
+    public Task CreateUser(CreateUserDto dto);
+}
