6fe532efa6f544071c17a973d2c1939754f83513
irmar <i.r.mark14@gmail.com>
2024-11-11 22:40:44 +0300

    (#47) add: добавил реализацию репозитория IntegrationRepository

---
+++ WatchHub/Infrastructure/Repositories/IntegrationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Exceptions;
+using IntegrationApplication;
+using IntegrationDomain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+internal class IntegrationRepository : IIntegrationRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public IntegrationRepository(ApplicationDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task<Integration> GetByMovieIdAsync(Guid id)
+    {
+        var integration = await _context.Integrations.FirstOrDefaultAsync(x => x.MovieId == id);
+        if (integration is null)
+        {
+            throw new NotFoundException($"Integration with this movie id {id} does not exist!");
+        }
+
+        return integration;
+    }
+}
