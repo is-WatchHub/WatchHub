efb0a130e07568e0bd3316c949638fcb5eebdc29
georgedem <georgiidemyanov@yandex.ru>
2024-11-12 21:56:01 +0300

    (#47) feat: movie platform association db model

---
+++ WatchHub/Infrastructure/Models/MoviePlatformAssociationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Models;
+
+public class MoviePlatformAssociationModel
+{
+    public Guid Id { get; set; }
+    public Guid IntegrationId { get; set; }
+    public Guid PlatformId { get; set; }
+    public string ExternalPlatformId { get; set; }
+    public virtual PlatformModel Platform { get; set; }
+    public virtual IntegrationModel Integration { get; set; }
+}
